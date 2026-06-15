dc77141e1c873d58f8e041b0ce6dfaa7e1d17fda
Stavros Nicolaou <stavros1000@outlook.com>
2026-06-15 20:21:20 +0300

    initial commit for hexamodulith GitHub template

---
+++ modules/inventory/inventory-core/src/main/java/com/example/inventory/test.java
@@ -0,0 +1,4 @@
+package com.example.inventory.adapters;
+
+public class test {
+}
---
+++ src/test/java/org/example/hexamodulith/HexamodulithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hexamodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexamodulithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
